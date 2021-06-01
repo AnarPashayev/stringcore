441ea98c49e12f07fe5ea2e5400a3d88e4cefc75
Anar Pashayev <anarpawayev@gmail.com>
2021-06-01 16:52:23 +0400

    next commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(1582, 10, 5);
+            Console.WriteLine(dateTime.DayOfWeek);
+
+        }
+    }
+}
